7b7c5580177f12cd732655fc0c0b97520384ffb6
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-29 12:31:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber (katzdDeli, person){
+  katzdDeli.push(person)
+  return (`Welcome, ${person}. You are number ${katzdDeli.length} in line.`)
+}
+
+function nowServing (katzdDeliLine){
+katzdDeliLine.shift()
+  
+  
+}
